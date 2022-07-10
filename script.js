b5915b3d540d7fb17f760feedb9d083afccb2348
Mike Winchel <mw0183163@otc.edu>
2022-07-10 04:03:26 -0500

    fixed JS error

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("titlebar").style.fontSize = "30px";
+    document.getElementById("header").style.fontSize = "30px";
   } else {
-    document.getElementById("titlebar").style.fontSize = "90px";
+    document.getElementById("header").style.fontSize = "90px";
   }
 }
